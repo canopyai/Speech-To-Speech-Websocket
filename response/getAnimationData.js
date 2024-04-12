0e5318b0de058f0722a1665d8dc014e8da36d827
Amartya Varma <amartya.varma@gmail.com>
2024-04-12 17:31:01 +0100

    testing sthg

--- response/getAnimationData.js
+++ response/getAnimationData.js
@@ -9,7 +9,7 @@ export const getAnimationData = async ({
 
         console.log("url", url)
 
-        if(text.length === 0) {
+        if(TTSSentence.length === 0) {
             console.log("No text to process");
             return;
         }
