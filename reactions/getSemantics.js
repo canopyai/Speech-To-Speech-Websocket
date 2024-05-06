b84dbae607ddfffb23df3962035aad0869ca1d70
Amartya Varma <amartya.varma@gmail.com>
2024-05-06 16:25:08 +0100

    f

--- reactions/getSemantics.js
+++ reactions/getSemantics.js
@@ -1,4 +1,5 @@
 export async function getSemantics({last3Messages, globals}) {
+    console.log(last3Messages)
     const url = "http://34.91.168.188:8080/classify"; // Replace with the appropriate URL
     console.log("attempting to get semantics")
 
