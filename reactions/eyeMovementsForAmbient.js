393acd00df1ff1cd9966b618243ebcc87e957748
Amartya Varma <amartya.varma@gmail.com>
2024-05-09 10:05:52 +0100

    F

--- reactions/eyeMovementsForAmbient.js
+++ reactions/eyeMovementsForAmbient.js
@@ -7,7 +7,7 @@ export function eyeMovementsForAmbient({
 
     const newVisemes = []
     for(let viseme of visemes){
-        console.log("viseme ", viseme)
+        console.log("viseme ", viseme, viseme.targets)
         const eyeTargets = [0,0,0,0]
         eyeTargets[3] = ratio1 * viseme.targets[0];
         eyeTargets[2] = ratio3 * visemes.targets[4];
