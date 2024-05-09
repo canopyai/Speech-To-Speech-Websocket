060bdcf0765215fea7cfb3f4d7e9f7eff5f91bf2
Amartya Varma <amartya.varma@gmail.com>
2024-05-09 10:16:56 +0100

    F

--- reactions/manageAmbientMovements.js
+++ reactions/manageAmbientMovements.js
@@ -18,7 +18,11 @@ export function manageAmbientMovements({
         console.log("manage ambient movements")
 
         globals.forwardSocket.ws.send(JSON.stringify({
-            visemes: eyeMovementsForAmbient({visemes}), 
+            // visemes: eyeMovementsForAmbient({visemes}), 
+            visemes:{
+                targets: [1,0,0,0,0,0,0,0,0,1], 
+                duration:15
+            },
             messageType:"ambientMovements"
         }))
         console.log()
