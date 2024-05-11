8d17a73c05e676bf2055cbadb828d44b31e4a033
Amartya Varma <amartya.varma@gmail.com>
2024-05-11 15:14:59 +0100

    F

--- reactions/getBrowsMovement.js
+++ reactions/getBrowsMovement.js
@@ -2,7 +2,7 @@ export function sendBrowsMovement({
     globals
 }) {
 
-    return;
+    // return;
     if(!globals.lastBrowMovement){
         globals.lastBrowMovement = Date.now()
     }
@@ -12,11 +12,21 @@ export function sendBrowsMovement({
     if(Math.random()>activationProbs) return;
 
     if(globals.forwardSocket && globals.forwardSocket.ws){
+        const prospectiveTargs = [Math.random(), Math.random(), Math.random(), Math.random()];
+
+        const sum = prospectiveTargs.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        
+        const strength = 0.5
+        const normalizedProspectiveTargs = prospectiveTargs.map(value => (value * strength) / sum);
+
+        
         globals.forwardSocket.ws.send(JSON.stringify({
+
+            
 
             visemes: [{
                 duration: 100, 
-                targets: [1,1,0,0]
+                targets: [1,0,0,0]
             }], 
             messageType:"brows"
    
