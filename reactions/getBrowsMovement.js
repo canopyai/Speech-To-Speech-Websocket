811d58e2c5fca8b3e5125c893a6d7736520914dd
Amartya Varma <amartya.varma@gmail.com>
2024-05-11 15:16:30 +0100

    F

--- reactions/getBrowsMovement.js
+++ reactions/getBrowsMovement.js
@@ -10,13 +10,14 @@ export function sendBrowsMovement({
     const activationProbs = 1
 
     if(Math.random()>activationProbs) return;
+    if(Date.now() - globals.lastBrowMovement<10000) return;
 
     if(globals.forwardSocket && globals.forwardSocket.ws){
         const prospectiveTargs = [Math.random(), Math.random(), Math.random(), Math.random()];
 
         const sum = prospectiveTargs.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         
-        const strength = 0.5
+        const strength = 0.1
         const normalizedProspectiveTargs = prospectiveTargs.map(value => (value * strength) / sum);
 
         
