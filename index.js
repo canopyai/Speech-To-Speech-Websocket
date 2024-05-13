596a34203d389e59b05671212a64ef5096ceb9f7
Amartya Varma <amartya.varma@gmail.com>
2024-05-13 14:36:56 +0100

    F

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ wss.on('connection', (ws, req) => {
     console.log("my ip address", ip)
  
 
-    if (ip == "94.252.122.131") {
+    if (ip == "94.252.122.131" ||ip == "35.204.82.115") {
         console.log("forward socket connected", ip)
         manageAmbientMovements({
             globals
