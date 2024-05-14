4a9f2100ec22ed67194af5b91e1aa473d10f0dc0
Amartya Varma <amartya.varma@gmail.com>
2024-05-14 19:56:13 +0100

    f

--- response/generateResponse.js
+++ response/generateResponse.js
@@ -64,7 +64,7 @@ export const generateResponse = async ({
     const contentWithoutString = lastMessage.content.replace(/<[^>]+>/g, '');
     lastMessage.content = contentWithoutString + `\n <${audioIntonationString}>`;
 
-    console.log("mainThreadCopy", mainThreadCopy)
+    // console.log("mainThreadCopy", mainThreadCopy)
     try {
 
 
