86f52c798c317af26849ed7a010a6f85836951e6
Amartya Varma <amartya.varma@gmail.com>
2024-05-19 22:54:44 +0100

    f

--- reactions/convertHeadToBody.js
+++ reactions/convertHeadToBody.js
@@ -1,5 +1,3 @@
-import { eyeMovementsForBody } from "./eyeMovementsForBody";
-
 export function convertHeadToBody({
     headVisemes
 }){
@@ -22,4 +20,30 @@ export function convertHeadToBody({
 
     return eyesIncluded
 
+}
+function eyeMovementsForBody({
+    visemes
+}) {
+
+
+    const lookUpRatio = 0.2 //index 5
+    const lookDownRatio = 0.45 //index 4
+    const lookLeftRatio = 0.3//index 0
+    const lookRightRatio = 0.3//index 1
+    const newVisemes = []
+    const timestep = 15
+    for (let viseme of visemes) {
+        const { targets } = viseme
+
+        let eyeTargets = [targets[5] * lookUpRatio, lookRightRatio * targets[1], targets[4] * lookDownRatio, targets[0] * lookLeftRatio]
+        
+
+        newVisemes.push({
+            targets: viseme.targets.concat(eyeTargets),
+            duration: (Math.random()<0.01)?((Math.random()*1500)):timestep
+        })
+
+    }
+
+    return newVisemes
 }
