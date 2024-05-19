ae2eb5dfb05bb7b335086d057524c2746a6f963b
Amartya Varma <amartya.varma@gmail.com>
2024-05-19 23:11:02 +0100

    f

--- utils/manageProcessingQueue.js
+++ utils/manageProcessingQueue.js
@@ -88,6 +88,7 @@ export const manageProcessingQueue = ({
             };
 
 
+            console.log("visemesWithEyes", visemesWithEyes)
             globals.forwardSocket.ws.send(JSON.stringify(SemotionAnimationData));
             globals.forwardSocket.ws.send(JSON.stringify(visemesWithEyes));
             globals.forwardSocket.ws.send(JSON.stringify(forwardData));
