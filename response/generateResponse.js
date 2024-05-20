2cae701b90e2eea50182afd898246802633890fd
Amartya Varma <amartya.varma@gmail.com>
2024-05-20 01:39:50 +0100

    f

--- response/generateResponse.js
+++ response/generateResponse.js
@@ -34,11 +34,11 @@ export const generateResponse = async ({
 
 
     let audioIntonationString;
-    if (globals.emotions.audioIntonation && globals.isAudioEmpathy) {
-    // if (globals.emotions.audioIntonation&&true) {
-        audioIntonationString = getAudioIntonationString(globals.emotions.audioIntonation.result);
+    // if (globals.emotions.audioIntonation && globals.isAudioEmpathy) {
+    // // if (globals.emotions.audioIntonation&&true) {
+    //     audioIntonationString = getAudioIntonationString(globals.emotions.audioIntonation.result);
 
-    }
+    // }
 
 
     let { hexCode } = generateRandomHex(pIdLength);
