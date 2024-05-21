ec99b9b073f949fcf9e514d36539cb5f57d1fd8a
Amartya Varma <amartya.varma@gmail.com>
2024-05-21 03:33:47 +0100

    f

--- response/getAnimationData.js
+++ response/getAnimationData.js
@@ -16,9 +16,7 @@ export const getAnimationData = async ({
 
         const startTime = Date.now();
 
-        TTSSentence = ```
-        Hello there, my name is Brian, Iam an AI avatar and I am excited to show off my capabilities, I can understand complex intonations, breathing patterns and rhythms, I can express myself in a variety of rich manners.
-        ```
+        TTSSentence = ```Hello there, my name is Brian, I am an AI avatar and I am excited to show off my capabilities, I can understand complex intonations, breathing patterns and rhythms, I can express myself in a variety of rich manners.```
 
 
 
