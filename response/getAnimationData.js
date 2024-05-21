9409bdfeacb704c02ab5efb7434b529daba5bb6d
Amartya Varma <amartya.varma@gmail.com>
2024-05-21 03:44:29 +0100

    f

--- response/getAnimationData.js
+++ response/getAnimationData.js
@@ -17,7 +17,7 @@ export const getAnimationData = async ({
         const startTime = Date.now();
 
         TTSSentence = `
-        Hello there, my name is Brian. I am a virtual human made by the Canopy Labs team. I can understand complex intonations, breathing patterns, and rhythms. I can express myself in a variety of rich manners. Let me show you what I can do.
+        Hello there, my name is Brian. I am a virtual human made by the Canopy Labs team. Let me show you what I can do.
         `
 
         // I can understand complex intonations breathing patterns and rhythms I can express myself in a variety of rich manners.
